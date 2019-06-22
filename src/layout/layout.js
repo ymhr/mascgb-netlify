677975c13268e56c695aa9de6db081befa90c0aa
James Martin <jamesmartin442@gmail.com>
2019-06-22 14:20:41 +0100

    Renames and stuff

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -11,7 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import { Container, Row, Col } from "react-grid-system"
 
 import "@/layout/layout.css"
-import Header from "../components/Header"
+import Header from "@/components/Header"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
