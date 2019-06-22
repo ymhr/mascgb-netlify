93a0ba0f9c63462947b860dccc639d78237b63d6
James Martin <jamesmartin442@gmail.com>
2019-06-22 14:59:45 +0100

    plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
